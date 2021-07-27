e927786bc6ba2d8471514207773af2ff9a19743f
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2021-07-27 13:45:52 +0200

    basic chat layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_messagewriter
+
+    def current_messagewriter
+        return @current_messagewriter if @current_messagewriter.present?
+        if session[:messagewriter_id].present?
+            @current_messagewriter = Messagewriter.find(session[:messagewriter_id])
+        else
+            @current_messagewriter = Messagewriter.generate
+            session[:messagewriter_id] = @current_messagewriter.id
+            @current_messagewriter
+        end
+    end
 end
--- app/models/messagewriter.rb
+++ app/models/messagewriter.rb
@@ -1,3 +1,9 @@
 class Messagewriter < ApplicationRecord
     validates_uniqueness_of :username
+
+    def self.generate
+        number = rand.to_s[2..4]
+        username = "User-#{number}"
+        create(username: username)
+      end
 end
