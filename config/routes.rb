0ddf68a15d91b5e9b0cc75ceb951e5619c771d8b
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2021-08-01 15:54:18 +0200

    rails routes management

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: "users/registrations" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: "users/registrations" }, skip: [:sessions, :registrations]
   
+  # changing the default paths
+  # When you are using only one role with Devise you may want to change the sign in and sign out routes to /login and /logout (instead of /users/sign_in and /users/sign_out).
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new', as: :new_user_session
+    post '/login', to: 'devise/sessions#create', as: :user_session
+    delete '/logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get '/signup', to: 'users/registrations#new', as: :new_user_registration
+
+  end
+
   resources :messages
   resources :rooms
   
-  root 'rooms#index'
-  
+  root 'pages#index'
+  get '/rooms', to: 'rooms#index', :as => :room_view
 
   #Admin Actions
   get '/adm', to: 'admins#index', :as => :admin_view
