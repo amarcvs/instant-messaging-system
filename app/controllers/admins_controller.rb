c545f04a58df068d23844422fe6397606177db25
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2021-08-01 15:57:54 +0200

    simple admin control panel added

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,30 +1,30 @@
-class AdminsController < AdminsController::Base
+class AdminsController < ApplicationController
 
     def index
         @users = User.all
     end
 
     def ban_user
-        u = User.find(params[id:])
+        u = User.find(params[:id])
         u.unset_messagewriter
         redirect_to :admin_view
     end
 
     def unban_user
-        u = User.find(params[id:])
+        u = User.find(params[:id])
         u.set_messagewriter
         redirect_to :admin_view
     end
 
     def admin_user
-        u = User.find(params[id:])
+        u = User.find(params[:id])
         u.set_messagewriter
         u.set_admin
         redirect_to :admin_view
     end
 
     def unadmin_user
-        u = User.find(params[id:])
+        u = User.find(params[:id])
         u.unset_admin
         redirect_to :admin_view
     end
