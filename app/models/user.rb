3fb905c8cece5a3f2af9bde495561e3e3cd002f0
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2021-09-19 12:22:02 +0200

    app-id and app-secret for access oAuth2 APIs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord #ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :confirmable, #:trackable,
+         :recoverable, :rememberable,# :confirmable, #:trackable,
          :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
          
 
@@ -25,7 +25,15 @@ class User < ApplicationRecord #ActiveRecord::Base
       end
     user
   end
-
+  
+  # Possible alternative of the above function
+  #def self.from_omniauth(auth)
+  #  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #    user.email = auth.info.email
+  #    user.password = Devise.friendly_token[0,20]
+  #  end
+  #end
+  
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
